92a0baa43d08389405d0d4468ffc611275079238
Jasper Reed <jmixon.reed@gmail.com>
2020-07-20 22:12:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,13 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value) final_array|
+    value.each do |names|
+      names.each do |name|
+        binding.pry
+      end
+    end
+    final_array
+  end
   binding.pry
 end
